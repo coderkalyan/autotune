169908fe6725df4cafa861104e32bdf86c0dd9d6
dudra <dudra@wisc.edu>
2026-02-28 17:26:32 -0600

    added files for audio control

---
+++ rtl/audio_clk_gen.sv
@@ -0,0 +1,17 @@
+module audio_clk_gen();
+    input logic i_clk_50M;
+    output logic o_clk_48K;
+    output logic o_clk_12_28M;
+    output logic o_clk_
+    output logic o_clk_400K;
+
+// PLL IP Instantiation 
+
+
+// 48kHz Divider Logic 
+
+
+//
+
+
+endmodule
