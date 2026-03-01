d1bc703e2656c131a0c83b59b6bf604c74c051b7
dudra <dudra@wisc.edu>
2026-03-01 16:34:42 -0600

    audio_cntrl shell + codec_fsm (not tested)

--- rtl/audio_cntrl.sv
+++ rtl/audio_cntrl.sv
@@ -1,14 +1,48 @@
 module audio_cntrl #(
-    parameter P24_BIT = 1
+    parameter P24_BIT = 1,
+    parameter DATA_WIDTH = P24_BIT ? 24 : 16
 )(
+    // FPGA facing IO
 
+    input logic i_clk_50M,                      // 50Mhz clk from FPGA
+    input logic i_rst,                          // Synchronous Active High Reset
+    input logic [DATA_WIDTH-1:0] i_data,        // Data to DAC (note; this input
+                                                //  must toggle if want diff data on 
+                                                //  left and right channels)
+    output logic o_send_over,                   // Flag finished sending data
+                                                //  to DAC (note; send left then right)
+    output logic o_recv_over,                   // Flag finished receiving data
+                                                //  from ADC (receive left then right)
+    output logic [DATA_WIDTH-1:0] o_left_data,  // Data from the ADC Left Channel
+    output logic [DATA_WIDTH-1:0] o_right_data, // Data from the ADC Right Channel
+    output logic o_config_err,                  // Flag indicating there was an error 
+                                                //  when configuring the Codec
+    output logic o_config_done,                 // Flag indicating done configuring Codec
 
+    // Codec facing IO
+
+    input logic i_aud_adcdat,                   // Data line from the ADC (I2S)
+    output logic o_aud_dacdat,                  // Data line from the DAC (I2S)
+    output logic o_bck,                         // Bit clock (I2S)
+    output logic o_aud_adclrck,                 // Word Select ADC (I2S)
+    output logic o_aud_daclrck,                 // Word Select DAC (I2S)
+    output logic o_aud_xck,                     // Codec Master Clock
+    output logic o_i2c_sclk,                    // Clock line (I2C)
+    output logic o_i2c_sdat                     // Data line to Codec (I2C)
 );
 
 ///////////////////////
 // INTERNAL SIGNALS  //
 ///////////////////////
 
+// TODO: see if there is an issue with clock domain crossing w the 50mhz
+//       and the bit clock (left and right data is flopped using bit clock,
+//       other modules will read using 50Mhz clk) -> Ask Abhishek since used PLL
+logic busy;
+logic done;
+logic start;
+logic addr;
+logic data;
 
 ///////////////////////
 // Clock Generation  //
@@ -16,9 +50,9 @@ module audio_cntrl #(
 audio_clk_gen #(
     .P24_BIT(P24_BIT)
 ) iCLKGEN(
-    .i_clk_50M(),
-    .i_rst(),
-    .o_clk_12_28M(),
+    .i_clk_50M(i_clk_50M),
+    .i_rst(i_rst),
+    .o_clk_12_28M(),                // Master Clock to Codec
     .o_clk_bit(),
     .o_clk_100K()
 );
@@ -29,12 +63,13 @@ audio_clk_gen #(
 i2s_receiver #(
     .P24_BIT(P24_BIT)
 ) iI2S_REC (
-    .i_sck(),               // Bit Clock 
-    .i_rst(),               // Synchronous Active high reset
-    .i_ws(),                // Work Line (from transmitter)
-    .i_sd(),                // Data line
-    .o_left_data(),         // Left Channel Data
-    .o_right_data()         // Right Channel Data
+    .i_sck(clk_bit),                // Bit Clock 
+    .i_rst(i_rst),                  // Synchronous Active high reset
+    .i_ws(),                        // Work Line (from transmitter)
+    .i_sd(),                        // Data line
+    .o_left_data(),                 // Left Channel Data
+    .o_right_data(),                // Right Channel Data
+    .o_recv_over()                  // Flag to indicate data word is ready (pulses for each channel)
 );
 
 
@@ -44,18 +79,30 @@ i2s_receiver #(
 i2s_transmitter #(
     .P24_BIT(P24_BIT)
 ) iI2S_TRANS (
-    .i_sck(),               // Bit Clock
-    .i_rst(),               // Synchronous Active high reset
-    .i_data(),              // Input data to send (Left 1st, right second)
-    .o_sd(),                // Data line
-    .o_ws(),                // Word select for Channel 1 vs Channel 2
-    .o_send_over()          // indicates finished sending data on 1 channel
+    .i_sck(),                       // Bit Clock
+    .i_rst(),                       // Synchronous Active high reset
+    .i_data(),                      // Input data to send (Left 1st, right second)
+    .o_sd(),                        // Data line
+    .o_ws(),                        // Word select for Channel 1 vs Channel 2
+    .o_send_over()                  // indicates finished sending data on 1 channel
 );
 
 ///////////////////////
 // CODEC CONFIG FSM  //
 ///////////////////////
-
+codec_fsm #(
+    .P24_BIT(P24_BIT)
+) iCONFIG (
+    .i_clk_50M(i_clk_50M),          // 50Mhz clk from FPGA
+    .i_rst(i_rst),                  // Synchronous Active High Reset
+    .i_busy(busy),                  // Busy Flag from I2C
+    .i_done(done),                  // Done Flag from I2C
+    .o_start_transaction(start),    // Start Transaction Flag to I2C
+    .o_addr(addr),                  // Register Addr to Configure
+    .o_data(data),                  // Data Configure Codec with
+    .o_config_done(o_config_done),  // Flag indicating Done configuring Codec
+    .o_config_err(o_config_err)     // Flag indicating error during configuration
+);
 
 
 ///////////////////////
--- rtl/codec_fsm.sv
+++ rtl/codec_fsm.sv
@@ -1,4 +1,223 @@
-module codec_fsm ();
+module codec_fsm #(
+    parameter P24_BIT = 1
+)(
+    input logic i_clk_50M,              // 50Mhz clk from FPGA 
+    input logic i_rst,                  // Synchronous Active High Reset
+    input logic i_busy,                 // Busy signal from I2C Master
+    input logic i_done,                 // Done signal from I2C Master
+    
+    output logic o_start_transaction,   // Flag to start transaction on I2C
+    output logic [6:0] o_addr,          // Address of register to configure
+    output logic [7:0] o_data,          //  
+    output logic o_config_done,         // Flag indicating configuration is done
+    output logic o_config_err         // Flag indicating error during configuration             
+);
 
+
+///////////////////////
+// PARAM DEFINITIONS //
+///////////////////////
+
+// MIC-IN CONFIGURATION
+localparam logic MICBOOST = 0;          // default
+localparam logic MUTEMIC = 0;
+localparam logic INSEL= 1;
+
+// ADC SW CONTROL
+localparam logic ADCHPD = 0;            // default
+localparam logic HPOR = 0;              // default
+
+// DAC SW CONTROL 
+localparam logic [1:0] DEEMPH = 2'd0;   // default
+localparam logic DACMU = 0;
+
+// OUTPUT SW CONTROL 
+localparam logic BYPASS = 0;
+localparam logic DACSEL = 1;
+localparam logic SIDETONE = 0;          // default
+
+// DIGITAL AUDIO INTERFACE CONTROL
+localparam logic FORMAT = 2'd2;         // default - I2C
+localparam logic IWL = P24_BIT ? 2'd2 : 2'd0;    
+localparam logic LRP = 0;               // default         
+localparam logic LRSWAP = 0;            // default
+localparam logic MS = 0;                // default
+localparam logic BCLKINV = 0;           // default
+
+// SAMPLE RATE CONTROL
+localparam logic NORMAL = 1;            // set to give 48khz sampling rate on 
+localparam logic BOSR = 0;              // ADC and DAC
+localparam logic [3:0] SR = 4'd0; 
+
+// ACTIVATING DIGITAL AUDIO INTERFACE
+localparam logic ACTIVE = 1;
+
+// POWER DOWN
+localparam logic LINEINPD = 1;          // default
+localparam logic MICPD = 0;
+localparam logic ADCPD = 0;
+localparam logic DACPD = 0;
+localparam logic OUTPD = 0;
+localparam logic OSCPD = 0;             // default
+localparam logic CLKOUTPD = 0;          // default
+localparam logic POWEROFF = 0;
+
+// REGISTERS AND DATA
+
+localparam logic [6:0] CODEC_ADDR = 7'h1A;  //CSB pin determines addr, tied to gnd
+
+localparam logic [6:0] ANALOG_PATH_CNTRL_ADDR = 7'h04;
+localparam logic [6:0] DIGITAL_PATH_CNTRL_ADDR = 7'h05;
+localparam logic [6:0] POWER_DOWN_CNTRL_ADDR = 7'h06;
+localparam logic [6:0] DIGITAL_INTERFACE_FORMAT_ADDR = 7'h07; 
+localparam logic [6:0] SAMPLE_CNTRL_ADDR = 7'h08;
+localparam logic [6:0] ACTIVE_CNTRL_ADDR = 7'h09;
+
+
+localparam logic [7:0] ANALOG_PATH_CNTRL_DATA = {
+    3'd0, 
+    SIDETONE,
+    DACSEL,
+    BYPASS,
+    INSEL,
+    MUTEMIC,
+    MICBOOST
+};
+localparam logic [7:0] DIGITAL_PATH_CNTRL_DATA= {
+    4'd3,
+    HPOR,
+    DACMU,
+    DEEMPH,
+    ADCHPD
+};
+localparam logic [7:0] POWER_DOWN_CNTRL_DATA= {
+    1'd0,
+    POWEROFF,
+    CLKOUTPD,
+    OSCPD,
+    OUTPD,
+    DACPD,
+    ADCPD,
+    MICPD,
+    LINEINPD
+};
+localparam logic [7:0] DIGITAL_INTERFACE_FORMAT_DATA= {
+    1'd0,
+    BCLKINV,
+    MS,
+    LRSWAP,
+    LRP,
+    IWL,
+    FORMAT
+};
+localparam logic [7:0] SAMPLE_CNTRL_DATA = {
+    3'd0,
+    SR, 
+    BOSR, 
+    NORMAL
+};
+localparam logic [7:0] ACTIVE_CNTRL_DATA= {
+    7'd0,
+    ACTIVE
+};
+
+localparam logic [7:0] REGISTER_DATA [0:5] = '{
+    ANALOG_PATH_CNTRL_DATA,
+    DIGITAL_PATH_CNTRL_DATA,
+    POWER_DOWN_CNTRL_DATA,
+    DIGITAL_INTERFACE_FORMAT_DATA,
+    SAMPLE_CNTRL_DATA,
+    ACTIVE_CNTRL_DATA
+};
+
+localparam logic [6:0] REGISTER_ADDR [0:5] = '{
+    ANALOG_PATH_CNTRL_ADDR,
+    DIGITAL_PATH_CNTRL_ADDR,
+    POWER_DOWN_CNTRL_ADDR,
+    DIGITAL_INTERFACE_FORMAT_ADDR,
+    SAMPLE_CNTRL_ADDR,
+    ACTIVE_CNTRL_ADDR
+};
+
+
+
+///////////////////////
+// STATE MACHINE     //
+///////////////////////
+
+
+logic [2:0] idx;
+logic inc;
+always @(posedge i_clk_50M) begin 
+    if (i_rst) 
+        idx <= 1'b0;
+    else if (inc)
+        idx <= idx + 1'b1;
+end
+
+typedef enum logic [1:0] {
+    LOAD,
+    WAIT, 
+    DONE, 
+    ERROR
+} state_t;
+
+state_t state, next_state;
+
+always @(posedge i_clk_50M) begin 
+    if (i_rst)
+        state = LOAD;
+    else 
+        state <= next_state;
+end 
+
+always_comb begin 
+    next_state = state;
+    o_start_transaction = 1'b0;
+    o_config_done = 1'b0;
+    o_config_err = 1'b0;
+    inc = 1'b0;
+    o_addr = 7'h00;
+    o_data = 8'h00;
+
+    case(state)
+        LOAD: begin 
+            // Set data and addr
+            o_addr = REGISTER_ADDR[idx];
+            o_data = REGISTER_DATA[idx];
+            // Wait until not busy, then start transaction
+            if (!i_busy) begin
+                o_start_transaction = 1'b1;
+                next_state = WAIT;
+            end
+        end
+        WAIT: begin 
+            // Hold Data Stable during configuration
+            o_addr = REGISTER_ADDR[idx];
+            o_data = REGISTER_DATA[idx];
+            // Wait until transaction is done,
+            if (i_done) begin 
+                if (idx == 3'd5) begin 
+                    next_state = DONE;
+                end else begin 
+                    inc = 1'b1;
+                    next_state = LOAD;
+                end  
+            end
+        end
+        DONE: begin 
+            o_config_done = 1'b1;
+            next_state = DONE;
+        end
+        ERROR: begin 
+            //TODO: an error flag should be inputted to the FSM from the 
+            //      I2C master (2 options: check for flag at every state,
+            //      else add flag to sensitivity list of state flop)
+            o_config_err = 1'b1;
+            next_state = ERROR;
+        end 
+        default: next_state = LOAD;
+    endcase
+end
 
 endmodule
