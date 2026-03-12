c52a8946ac604d10c317f71d566f01700bc3fef0
Vishrut Agrawal <zvishrut@gmail.com>
2026-03-12 16:10:36 -0500

    Updated circular buffer to use memory and IPs

--- rtl/autocorrelation/circular_buffer.sv
+++ rtl/autocorrelation/circular_buffer.sv
@@ -9,42 +9,70 @@ module circular_buffer #(
   input clk,
   input rst,
   input i_wr_en,
-  input i_rd_en,
   input [DATA_WIDTH-1:0] i_wr_data,
   input i_inc_rd_ptr, // increment rd_ptr by 1
   input [9:0] i_rd_addr, // 0-1024, translated to real address in buffer
-  output reg [DATA_WIDTH-1:0] o_rd_data,
-  output reg o_data_vld
+  output reg [DATA_WIDTH-1:0] o_data
 );
   localparam SEGMENT_SIZE = 256;
   localparam NUM_SEGMENTS = 5;
   localparam BUFFER_SIZE = SEGMENT_SIZE * NUM_SEGMENTS;
   localparam SEG4_BASE = 1024;
+  
+  logic [10:0] wr_ptr, rd_ptr, rd_addr;
 
-  logic [DATA_WIDTH-1:0] buffer [0:1279]; // 1280 data points, 16 bits per data point
-
-  logic [10:0] wr_ptr, rd_ptr;
+  `ifdef SIM
+  memory #(
+    .P24_BIT(P24_BIT),
+    .ADDR_WIDTH(11)
+  ) memory_inst (
+    .i_clk(clk),
+    .i_data(i_wr_data),
+    .i_wr_addr(wr_ptr),
+    .i_rd_addr(rd_addr),
+    .i_wr_en(i_wr_en),
+    .o_data(o_data)
+  );
+  `else
+  generate
+    if (P24_BIT) begin
+      bram_24_2k bram_24_2k_inst (
+        .clock(clk),
+        .data(i_wr_data),
+        .rdaddress(rd_addr),
+        .wraddress(wr_ptr),
+        .wren(i_wr_en),
+        .q(o_data)
+      );
+    end else begin
+      bram_16_2k bram_16_2k_inst (
+        .clock(clk),
+        .data(i_wr_data),
+        .rdaddress(rd_addr),
+        .wraddress(wr_ptr),
+        .wren(i_wr_en),
+        .q(o_data)
+      );
+    end
+  endgenerate
+  `endif
 
   always_ff @(posedge clk) begin
     if (rst) begin
       rd_ptr <= '0;
       wr_ptr <= '0;
-      o_rd_data <= '0;
-      o_data_vld <= 0;
     end else begin
-      o_data_vld <= 0;
       if (i_inc_rd_ptr) begin
         rd_ptr <= rd_ptr == SEG4_BASE ? 0 : rd_ptr + SEGMENT_SIZE;
       end
       if (i_wr_en) begin
-        buffer[wr_ptr] <= i_wr_data;
         wr_ptr <= wr_ptr == BUFFER_SIZE - 1 ? 0 : wr_ptr + 1;
       end
-      if (i_rd_en) begin
-        o_rd_data <= buffer[(i_rd_addr + rd_ptr) >= BUFFER_SIZE ? (i_rd_addr + rd_ptr) - BUFFER_SIZE : (i_rd_addr + rd_ptr)];
-        o_data_vld <= 1;
-      end
     end
+  end
+
+  always_comb begin
+    rd_addr = (i_rd_addr + rd_ptr) >= BUFFER_SIZE ? (i_rd_addr + rd_ptr) - BUFFER_SIZE : (i_rd_addr + rd_ptr);
   end
 
 endmodule
