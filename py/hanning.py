4a67be03e66c69c2c524967f8a82035139180d97
Kalyan Sriram <kalyan@coderkalyan.com>
2026-04-04 21:09:09 -0500

    initial fixed rate psola passthrough

--- py/hanning.py
+++ py/hanning.py
@@ -1,13 +1,18 @@
 import numpy as np
 
 N = 4096
-window = np.hanning(N)
-normalized_window = window / np.max(window)
+n = np.arange(N)
+window = 0.5 - 0.5 * np.cos(2 * np.pi * n / N)
+normalized_window = window
+# window = np.hanning(N)
+# normalized_window = window / np.max(window)
 
 # Convert to Q11.16: Scale by 2^16 and convert to integer
 # 11 bits for integer (including sign), 16 bits for fraction
 scale = 1 << 16
 fixed_point_values = (normalized_window * scale).astype(np.int32)
+
+print(window + np.roll(window, N//2))  # should be all 1.0s
 
 with open("hanning.hex", "w") as f:
     for val in fixed_point_values:
