ca3bdedef54417b3e9513e03cfe8ce0b7e362dfb
Kalyan Sriram <kalyan@coderkalyan.com>
2026-04-04 23:15:11 -0500

    integrate hanning_var

--- rtl/hanning.sv
+++ rtl/hanning.sv
@@ -1,4 +1,4 @@
-`timescale 1ns/1ps
+`timescale 1ns / 1ps
 
 `include "fixed.sv"
 
@@ -6,13 +6,14 @@ module hanning #(
     parameter int N = 4096,
     parameter int B = $clog2(N)
 ) (
-    input  wire           clk,
-    input  wire           rst,
-    input  wire [B - 1:0] i_index,
-    output fixed_t        o_data
+    input  wire              clk,
+    input  wire              rst,
+    input  wire    [B - 1:0] i_index,
+    output fixed_t           o_data
 );
-    logic [31:0] rom [N];
-    initial $readmemh("hanning.hex", rom);
+  logic [31:0] rom[N];
+  // initial $readmemh("hanning.mem", rom);
+  initial $readmemh("/home/kalyan/Documents/school/ece554/autotune/rtl/hanning.mem", rom);
 
-    always_comb o_data = fixed_t'(rom[i_index][26:0]);
+  always_comb o_data = fixed_t'(rom[i_index][26:0]);
 endmodule
