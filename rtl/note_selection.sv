7bb59435455493efb9fb888dcb73be213a1fb3f3
dudra <dudra@wisc.edu>
2026-04-09 17:39:17 -0500

    working midi integration w keyboard

--- rtl/note_selection.sv
+++ rtl/note_selection.sv
@@ -1,13 +1,17 @@
+import global_enums::*;
 `include "fixed.sv"
 
 module note_selection (
     input [9:0] actual_lag,
-    input [9:0] target_lag,
+    input [9:0] target_lag, 
+    input mode_t mode,
     output fixed_t shift_ratio
 );
 
 /*
-Shift ratio = f(actual) / f(target) 
+Shift ratio = 1 / PF
+            = 1 / (f(target) / f(actual))
+            = f(actual) / f(target) 
             = lag(new) / lag(actual)
             = lag(new) * 1 / lag(actual)
             
@@ -60,7 +64,14 @@ fixed_t target_fixed;
 
 assign target_fixed = fixed_t'({1'b0,target_eff,16'd0});
 assign shift_ratio = fixed_mul(target_fixed, reciprocal);
-assign target_eff = (target_lag == 0) ? nearest_note : target_sat;
+
+always_comb begin 
+    case (mode)
+        AUTOTUNE: target_eff = nearest_note;
+        VOCODE: target_eff = target_sat;
+        default: target_eff = actual_eff; // default to passthrough  
+    endcase
+end
 
 
 endmodule
