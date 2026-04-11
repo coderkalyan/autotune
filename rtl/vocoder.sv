b275fc7bc1319dd425daaae1e0af16c02467f260
Jonathon Wang <jwang2672@wisc.edu>
2026-04-10 19:29:13 -0500

    vocoder can play multiple notes

--- rtl/vocoder.sv
+++ rtl/vocoder.sv
@@ -5,7 +5,7 @@
 // a_att: float = 1.0 - np.exp(-1.0 / (attack_ms  * 1e-3 * fs))
 // a_rel: float = 1.0 - np.exp(-1.0 / (release_ms * 1e-3 * fs))
 module vocoder #(
-    parameter int N = 1091,
+    parameter int N = 7438,
     parameter int B = $clog2(N),
     parameter int attack_ms = 3,  //alpha attack
     parameter int release_ms = 100  //alpha attack
@@ -18,24 +18,33 @@ module vocoder #(
     output logic   o_valid
 );
     audio_t rom[N];
-    initial $readmemh("sawtooth.mem", rom);
+    audio_t idx_rom[41];
+    // initial $readmemh("sawtooth440.mem", rom);
+    //first note is G#2 MIDI 44
+    //A4 is MIDI 69
+    initial $readmemh("sawtooth_total.mem", rom);
+    initial $readmemh("sawtooth_start_idx.mem", idx_rom);
 
-    logic [15:0] idx;
+    logic [16:0] idx;
+    logic [16:0] idx2;
+    logic [16:0] note;
 
     always_ff @(posedge clk) begin
         if (rst) begin
             o_valid <= 1'b0;
-            idx     <= '0;
+            idx     <= idx_rom[25];
+            idx2     <= idx_rom[29];
         end else begin
             o_valid <= i_valid;
             if (i_valid) begin
-                idx <= ((idx >= (N - 1))) ? '0 : (idx + 1);
+                idx <= ((idx >= (idx_rom[26] - 1))) ? idx_rom[25] : (idx + 1);
+                idx2 <= ((idx2 >= (idx_rom[30] - 1))) ? idx_rom[29] : (idx2 + 1);
             end
         end
     end
 
     // assign o_data = rom[idx];
-    assign o_data = fixed_atof(rom[idx]) >> 3;
+    assign o_data = (fixed_atof(rom[idx]) >> 4) + (fixed_atof(rom[idx2] >> 4));
     // assign o_raw = rom[idx];
     assign o_raw  = idx;
 
